35e337c6d27fd932b53b236bf2d594be8679e3f8
jedklika <jedklika@gmail.com>
2025-02-03 16:27:55 -0500

    Merged my stuff into main
    also fixed the UI screens for the menus

--- Hubble Bubble/Assets/Code/Collisions.cs
+++ Hubble Bubble/Assets/Code/Collisions.cs
@@ -1,44 +1,148 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Collisions : MonoBehaviour
 {
-    [SerializeField] bool isGlassBubble;
-    [SerializeField] bool isMetalBubble;
-    [SerializeField] bool isPlasticBubble;
-    // Start is called before the first frame update
+    GameManager gm;
+    UI um;
+
+    public int hits;
+    private void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+        um = FindObjectOfType<UI>();
+    }
+    private void Update()
+    {
+        if (gm.ItemsCarried >= gm.GlassMaxCarryBuff && gm.BubbleType == "Glass" || gm.ItemsCarried >= gm.MetalMaxCarryCap && gm.BubbleType == "Metal" || gm.ItemsCarried >= gm.PlasticMaxCarryCap && gm.BubbleType == "Plastic")
+        {
+            gm.Full = true;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Glass" && !isMetalBubble || !isPlasticBubble) 
+        if (other.tag == "Glass" && !gm.Assigned)
         {
-            isGlassBubble = true;
-            isPlasticBubble = false;
-            isMetalBubble = false;
-            if (!isMetalBubble || !isPlasticBubble)
-            {
-                Destroy(other.gameObject);
-            }
+
+            um.AddCarryItems();
+            gm.InPlayer.Add(other.gameObject);
+            gm.Speed = gm.Speed - gm.GlassSpeedDebuff;
+            gm.BubbleType = "Glass";
+            gm.Assigned = true;
+            other.gameObject.SetActive(false);
         }
-        else if (other.tag == "Metal" && !isGlassBubble || !isPlasticBubble)
+        else if (other.tag == "Glass" && gm.BubbleType == "Glass" && !gm.Full)
         {
-            isMetalBubble = true;
-            isGlassBubble = false;
-            isPlasticBubble = false;
-            if(!isGlassBubble || !isPlasticBubble) 
-            { 
-                Destroy(other.gameObject); 
-            }
+
+            um.AddCarryItems();
+            gm.InPlayer.Add(other.gameObject);
+            gm.Speed = gm.Speed - gm.GlassSpeedDebuff;
+            gm.BubbleType = "Glass";
+            gm.Assigned = true;
+            other.gameObject.SetActive(false);
         }
-        else if (other.tag == "Plastic" && !isGlassBubble || !isMetalBubble)
+        else if (gm.BubbleType == "Glass" && other.tag == "Dump")
         {
-            isPlasticBubble = true;
-            isMetalBubble = false;
-            isGlassBubble = false;
-            if (!isGlassBubble || !isMetalBubble)
-            {
-                Destroy(other.gameObject);
-            }
+            um.DisposeGlass();
+            DiposeObjectsInPlayer();
+            gm.AddGlass();
+            gm.Durability--;
         }
+        else if (gm.BubbleType == "Glass" && other.tag != "Glass" && other.tag != "Dump")
+        {
+            RestoreObjectsInPlayer();
+
+            gm.Durability--;
+
+        }
+        else if (other.tag == "Metal" && !gm.Assigned)
+        {
+            um.AddCarryItems();
+            gm.InPlayer.Add(other.gameObject);
+            gm.LifeCurrTime += 5;
+            gm.Speed = gm.Speed - gm.MetalSpeedDeBuff;
+            gm.BubbleType = "Metal";
+            gm.Assigned = true;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Metal" && gm.BubbleType == "Metal" && !gm.Full)
+        {
+            um.AddCarryItems();
+            gm.InPlayer.Add(other.gameObject);
+            gm.LifeCurrTime += 5;
+            gm.Speed = gm.Speed - gm.MetalSpeedDeBuff;
+            gm.BubbleType = "Metal";
+            gm.Assigned = true;
+            other.gameObject.SetActive(false);
+        }
+        else if (gm.BubbleType == "Metal" && other.tag == "Dump")
+        {
+            um.DisposeMetal();
+            DiposeObjectsInPlayer();
+            gm.AddMetal();
+            gm.Durability--;
+        }
+        else if (gm.BubbleType == "Metal" && other.tag != "Metal" && other.tag != "Dump")
+        {
+            RestoreObjectsInPlayer();
+            Debug.Log("Die2");
+            gm.Durability--;
+
+        }
+        else if (other.tag == "Plastic" && !gm.Assigned)
+        {
+            um.AddCarryItems();
+            gm.InPlayer.Add(other.gameObject);
+            gm.Speed = gm.Speed + gm.PlasticSpeedBuff;
+            gm.BubbleType = "Plastic";
+            gm.Assigned = true;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Plastic" && gm.BubbleType == "Plastic" && !gm.Full)
+        {
+            um.AddCarryItems();
+            gm.InPlayer.Add(other.gameObject);
+            gm.BubbleType = "Plastic";
+            gm.Speed = gm.Speed + gm.PlasticSpeedBuff;
+            gm.Assigned = true;
+            other.gameObject.SetActive(false);
+        }
+        else if (gm.BubbleType == "Plastic" && other.tag == "Dump")
+        {
+            um.DisposePlastic();
+            DiposeObjectsInPlayer();
+            gm.AddPlastic();
+            gm.Durability--;
+        }
+        else if (gm.BubbleType == "Plastic" && other.tag != "Plastic" && other.tag != "Dump")
+        {
+            RestoreObjectsInPlayer();
+            Debug.Log("Die3");
+            gm.Durability--;
+
+        }
+        else if (gm.Full)
+        {
+            RestoreObjectsInPlayer();
+            Debug.Log("Die4");
+            gm.Durability--;
+
+        }
+
+    }
+    public void DiposeObjectsInPlayer()
+    {
+        gm.InPlayer.Clear();
+    }
+    public void RestoreObjectsInPlayer()
+    {
+        foreach (GameObject item in gm.InPlayer)
+        {
+            item.SetActive(true);
+        }
+        gm.InPlayer.Clear();
     }
 }
--- Hubble Bubble/Assets/Code/GameManager.cs
+++ Hubble Bubble/Assets/Code/GameManager.cs
@@ -1,57 +1,308 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] float carryCap;
-    [SerializeField] float itemsCarried;
+    [SerializeField] float maxCarryCap;
+    [SerializeField] int itemsCarried;
     [SerializeField] float speed;
-    [SerializeField] float durability;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float mimSpeed;
+    [SerializeField] bool isFull;
 
+    [SerializeField] int durability;
     [SerializeField] int lives;
+    [SerializeField] int maxLives;
     [SerializeField] float lifeTime;
 
     [SerializeField] int plasticAmount;
     [SerializeField] int metalAmount;
     [SerializeField] int glassAmount;
+    [SerializeField] int maxPlasticAmount;
+    [SerializeField] int maxMetalAmount;
+    [SerializeField] int maxGlassAmount;
 
-    public float CarryCap 
+    [SerializeField] float glassMaxCarryBuff;
+    [SerializeField] float glassSpeedDebuff;
+
+    [SerializeField] float metalMaxCarryCap;
+    [SerializeField] float metalSpeedDeBuff;
+
+    [SerializeField] float plasticMaxCarryCap;
+    [SerializeField] float plasticSpeedBuff;
+
+    [SerializeField] string bubbleType;
+    [SerializeField] bool assigned;
+
+    [SerializeField] Transform respawnPoint;
+    [SerializeField] Transform player;
+
+    [SerializeField] List<GameObject> inPlayer = new List<GameObject>();
+    [SerializeField] bool hasWon;
+    [SerializeField] float maxTime;
+
+    UI um;
+
+    public float MaxCarryCap
     {
-        get { return carryCap; }
+        get { return maxCarryCap; }
+        set { maxCarryCap = value; }
     }
-    public float ItemsCarried
+    public int ItemsCarried
     {
         get { return itemsCarried; }
+        set { itemsCarried = value; }
     }
     public float Speed
     {
         get { return speed; }
+        set { speed = value; }
     }
-    public float Durability
+    public float MaxSpeed
+    {
+        get { return maxSpeed; }
+        set { MaxSpeed = value; }
+    }
+    public float MimSpeed
+    {
+        get { return mimSpeed; }
+        set { mimSpeed = value; }
+    }
+    public int Durability
     {
         get { return durability; }
+        set { durability = value; }
     }
 
     public int Lives
     {
         get { return lives; }
+        set { lives = value; }
     }
-    public float LifeTime 
+    public float LifeTime
     {
         get { return lifeTime; }
+        set { lifeTime = value; }
     }
 
-    public int PlasticAmount 
-    { 
-        get { return plasticAmount; }   
+    public int PlasticAmount
+    {
+        get { return plasticAmount; }
+        set { plasticAmount = value; }
+    }
+    public int MaxPlasticAmount
+    {
+        get { return maxPlasticAmount; }
+        set { maxPlasticAmount = value; }
     }
     public int MetalAmount
     {
         get { return metalAmount; }
+        set { metalAmount = value; }
+    }
+    public int MaxMetalAmount
+    {
+        get { return maxMetalAmount; }
+        set { maxMetalAmount = value; }
     }
     public int GlassAmount
     {
         get { return glassAmount; }
+        set { glassAmount = value; }
+    }
+    public int MaxGlassAmount
+    {
+        get { return maxGlassAmount; }
+        set { maxGlassAmount = value; }
+    }
+
+    public string BubbleType
+    {
+        get { return bubbleType; }
+        set { bubbleType = value; }
+    }
+
+    public bool Assigned
+    {
+        get { return assigned; }
+
+        set { assigned = value; }
+    }
+
+    public bool Full
+    {
+        get { return isFull; }
+
+        set { isFull = value; }
+    }
+
+    public float GlassMaxCarryBuff
+    {
+        get { return glassMaxCarryBuff; }
+
+        set { glassMaxCarryBuff = value; }
+    }
+
+    public float MetalMaxCarryCap
+    {
+        get { return metalMaxCarryCap; }
+
+        set { metalMaxCarryCap = value; }
+    }
+
+    public float PlasticMaxCarryCap
+    {
+        get { return plasticMaxCarryCap; }
+
+        set { plasticMaxCarryCap = value; }
+    }
+    public float GlassSpeedDebuff
+    {
+        get { return glassSpeedDebuff; }
+
+        set { glassSpeedDebuff = value; }
+    }
+
+    public float MetalSpeedDeBuff
+    {
+        get { return metalSpeedDeBuff; }
+
+        set { metalSpeedDeBuff = value; }
+    }
+
+    public float PlasticSpeedBuff
+    {
+        get { return plasticSpeedBuff; }
+
+        set { plasticSpeedBuff = value; }
+    }
+
+    public List<GameObject> InPlayer
+    {
+        get { return inPlayer; }
+
+        set { inPlayer = value; }
+    }
+
+    public bool Won
+    {
+        get { return hasWon; }
+
+        set { hasWon = value; }
+    }
+
+    public float LifeCurrTime;
+    private bool _waitingForRespawn;
+
+    private void Start()
+    {
+        um = FindObjectOfType<UI>();
+        respawnPoint = GameObject.Find("Respawn Point").transform;
+        player = GameObject.Find("Player").transform;
+        _waitingForRespawn = true;
+        LifeCurrTime = LifeTime;
+        //StartCoroutine(deathTimer());
+
+    }
+    private void Update()
+    {
+        switch (bubbleType)
+        {
+            case "Glass":
+                maxCarryCap = glassMaxCarryBuff;
+                um.GlassActive();
+                break;
+            case "Metal":
+                maxCarryCap = metalMaxCarryCap;
+                um.MetalActive();
+                break;
+            case "Plastic":
+                maxCarryCap = plasticMaxCarryCap;
+                um.PlasticActive();
+                break;
+            default:
+                um.CarriedItems.text = "0 /";
+                um.BubbleType.color = Color.white;
+                um.MaxCarryCap.text = "0";
+                break;
+        }
+        if (durability <= 0)
+        {
+            Respawn();
+            um.UpadeHealthUI();
+        }
+        if (lives <= 0 && !hasWon)
+        {
+            Die();
+        }
+
+        if (speed >= maxSpeed)
+        {
+            speed = maxSpeed;
+        }
+        else if (speed <= mimSpeed)
+        {
+            speed = mimSpeed;
+        }
+        if (glassAmount == maxGlassAmount && metalAmount == maxMetalAmount && plasticAmount == maxPlasticAmount)
+        {
+            Debug.Log("Win");
+            hasWon = true;
+        }
+        if (_waitingForRespawn)
+        {
+            LifeCurrTime -= Time.deltaTime;
+            if (LifeCurrTime < 0.0f)
+            {
+                _waitingForRespawn = false;
+                Respawn();
+            }
+        }
+    }
+    public void Respawn()
+    {
+        StopAllCoroutines();
+        durability = 1;
+        bubbleType = null;
+        isFull = false;
+        player.position = respawnPoint.position;
+        lives--;
+        speed = 10;
+        itemsCarried = 0;
+        maxCarryCap = 1;
+        assigned = false;
+        _waitingForRespawn = true;
+        LifeCurrTime = LifeTime;
+        //StartCoroutine(deathTimer());
+        Debug.Log("Respawned");
+    }
+    public void Die()
+    {
+        Debug.Log("Die");
+        SceneManager.LoadScene(0);
+    }
+    //IEnumerator deathTimer() 
+    //{
+    //        yield return new WaitForSeconds(lifeTime);
+    //        Respawn();
+    //}
+    public void AddPlastic()
+    {
+        plasticAmount = plasticAmount + itemsCarried;
+    }
+
+    public void AddMetal()
+    {
+        metalAmount = metalAmount + itemsCarried;
+    }
+
+    public void AddGlass()
+    {
+        glassAmount = glassAmount + itemsCarried;
     }
 }
+
